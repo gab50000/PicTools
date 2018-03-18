f91da0c857775514db8197b6221410ef8d1a40d2
gk <gabriel.kabbe@mail.de>
2018-03-18 18:57:14 +0100

    Move doc into main

--- pic_import.py
+++ pic_import.py
@@ -1,17 +1,4 @@
 #!/usr/bin/env python3
-"""
-Import Pictures from card and save them in folders, one for each day
-
-Usage: 
-    test.py [--destination <dest>] [--move] [--type <type>] <source>
-    test.py (-h|--help)
-
-Options:
-  -h --help                   show this help message and exit
-  --destination <dest>        Destination folder [default: /home/kabbe/Bilder/Olympus]
-  --type <type>               Picture type [default: jpg]
-  --move                      Move pictures instead of copying them [default: False]
-"""
 from docopt import docopt
 import time
 import os
@@ -21,18 +8,28 @@ import ipdb
 
 
 def main(*args):
-    args = docopt(__doc__)
-    print(args)
-    
+    """
+    Import Pictures from card and save them in folders, one for each day
+
+    Usage:
+        pic_import.py [options] <source>
+        pic_import.py (-h|--help)
+
+    Options:
+        -h --help                 Show this help message and exit
+        --destination <dest>      Destination folder [default: /home/kabbe/Bilder/Olympus]
+        --type <type>, -t <type>  Picture type [default: jpg]
+        --move                    Move pictures instead of copying them [default: False]
+    """
+    args = docopt(main.__doc__)
+
     ending_length = len(args["--type"])
     pics = [os.path.join(args["<source>"], f) for f in os.listdir(args["<source>"]) if f[-ending_length-1:].upper() == ".{}".format(args["--type"].upper())]
-    
+
     print(len(pics), "pics found")
-    
-    #~ for pic in pics:
-        
+
     dates = sorted(list(set([time.strftime("%Y-%m-%d", time.gmtime(os.path.getctime(pic))) for pic in pics])))
-    
+
     for date in dates:
         save_dir = os.path.join(args["--destination"], date, args["--type"])
         if os.path.exists(save_dir):
@@ -40,12 +37,12 @@ def main(*args):
         else:
             print("I will create a directory", save_dir)
             os.makedirs(save_dir)
-            
+
     if args["--move"]:
         print("Moving files...")
     else:
         print("Copying files...")
-    
+
     for pic in pics:
         date_path = time.strftime("%Y-%m-%d", time.gmtime(os.path.getctime(pic)))
         last_slash_index = pic.rfind("/")
@@ -61,6 +58,6 @@ def main(*args):
                 print("copying file {} to {}".format(pic[last_slash_index+1:], dest_path))
                 shutil.copy2(pic, dest_path)
 
-    
+
 if __name__ == "__main__":
     main()
